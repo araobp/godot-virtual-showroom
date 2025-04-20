46ccd60ab9ef3fe8caf37ee7f643b0347c236f50
araobp <araobp@users.noreply.github.com>
2025-04-21 00:35:18 +0900

    update

--- 3dtest/vr_theater.gd
+++ 3dtest/vr_theater.gd
@@ -1,19 +1,40 @@
 extends Node3D
 
+var image_resources = [
+	"IMG_0227_level.jpg",
+	"IMG_0230_level.jpg",
+	"IMG_0232_level.jpg",
+	"IMG_0236_level.jpg",
+	"IMG_0238_level.jpg"	
+]
+
+var idx = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	set_image(0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+func set_image(idx):
+	var material = StandardMaterial3D.new()
+	var image = load("res://Panorama/" + image_resources[idx])
+	material.albedo_texture = image
+	material.emission_texture = image
+	material.emission_enabled = true
+	$SCREEN.set_surface_override_material(0, material)
 
 func _on_button_image_backward_button_down() -> void:
-	pass # Replace with function body.
+	if idx > 0:
+		idx -= 1
 
-
+	set_image(idx)		
+	
 func _on_button_image_forward_button_down() -> void:
-	pass # Replace with function body.
+	if idx < 4:
+		idx += 1
+
+	set_image(idx)
