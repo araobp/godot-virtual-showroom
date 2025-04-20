d3acc6fb871649ace1a9029921de9697fa8e7bec
araobp <araobp@users.noreply.github.com>
2025-04-20 20:55:58 +0900

    update

---
+++ 3dtest/vr_theater.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_image_backward_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_image_forward_button_down() -> void:
+	pass # Replace with function body.
