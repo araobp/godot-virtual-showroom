3b2d1b332b65576456b3f5ee998b47d646e3bba1
araobp <araobp@users.noreply.github.com>
2025-05-21 01:59:40 +0900

    update

---
+++ root_motion/walk_with_root.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	print($AnimationTree.get_root_motion_position())
+	var pos = $AnimationTree.get_root_motion_position()
+	pos = Vector3(pos.x, pos.z, pos.y)
+	$Armature.global_position += pos
+	
